81da2d63dc619bcab69c9e8ed00b75a3c0c6224a
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-09-18 04:14:34 -0700

    my-chat-app

---
+++ app.js
@@ -0,0 +1,5 @@
+// Initialize Supabase
+// const SUPABASE_URL = "https://pwjvisderoyqbbxifzrw.supabase.co";
+// const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3anZpc2Rlcm95cWJieGlmenJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMzM3MzksImV4cCI6MjA3MTYwOTczOX0.eesSF4krIK1HkNNwjoIsy6bOk2MS73CKCaUV__T4ewA";
+// const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
